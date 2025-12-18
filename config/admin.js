a8c633126b25fc2ea4706b01852008df47b3d753
Jonas Blum <jonasblum@mac.lan>
2025-12-18 18:30:10 -0500

    for fix

--- config/admin.js
+++ config/admin.js
@@ -10,11 +10,4 @@ module.exports = ({ env }) => ({
   apiToken: {
     salt: env("API_TOKEN_SALT"),
   },
-module.exports = ({ env }) => ({
-  // ...
-  transfer: {
-    token: {
-      salt: env("TRANSFER_TOKEN_SALT"),
-    },
-  },
 });
