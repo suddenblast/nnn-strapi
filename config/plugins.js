39ba0ef597c0578f0f0c9c9548f94d10e3fe917a
Jonas Blum <jonasblum@mac.lan>
2025-12-18 18:05:13 -0500

    Add users-permissions jwtSecret config (js)

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,11 @@
-export default ({ env }) => ({
+// config/plugins.js
+module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
-      jwtSecret: env("USERS_PERMISSIONS_JWT_SECRET"),
+      jwtSecret:
+        env("USERS_PERMISSIONS_JWT_SECRET") ||
+        env("JWT_SECRET") ||
+        env("ADMIN_JWT_SECRET"),
     },
   },
 });
